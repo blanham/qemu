ffb0652ef5d2b66f31a1ac1cc2df73407f3abe3c
Bryce Lanham <blanham@gmail.com>
2026-08-24 23:31:10 -0500

    hw/display: include the current IRQ interface

--- hw/display/bcm2835_hdmi.c
+++ hw/display/bcm2835_hdmi.c
@@ -11,7 +11,7 @@
 
 #include "qemu/osdep.h"
 #include "hw/display/bcm2835_hdmi.h"
-#include "hw/irq.h"
+#include "hw/core/irq.h"
 #include "migration/vmstate.h"
 #include "qemu/log.h"
 #include "qemu/module.h"
