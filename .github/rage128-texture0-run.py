fafd0e38245ffe24b731f6d7634e3ee986ffec21
Bryce Lanham <blanham@gmail.com>
2026-08-26 04:37:38 -0500

    ci: fix texture patch driver quoting

--- .github/rage128-texture0-run.py
+++ .github/rage128-texture0-run.py
@@ -16,20 +16,20 @@ end_marker = (
 )
 start = code.index(start_marker)
 end = code.index(end_marker, start) + len("\n)\n")
-replacement = r'''
-replace_regex_once(
-    source,
-    r"(    if \(ctx->depth_enabled &&\n"
-    r"        !ati_3d_depth_surface\(s, &ctx->depth_surface, &ctx->depth_mask,\n"
-    r"                              &ctx->depth_function\)\) \{\n"
-    r".*?\n    \})\n    return true;",
-    r'''\1
-    ctx->texture_enabled = ctx->tex_control & ATI_3D_TEXMAP_ENABLE;
-    if (ctx->texture_enabled && !ati_3d_texture_init(ctx)) {
-        return false;
-    }
-    return true;''',
+replacement = (
+    "\nreplace_regex_once(\n"
+    "    source,\n"
+    "    r\"(    if \\(ctx->depth_enabled &&\\n\"\n"
+    "    r\"        !ati_3d_depth_surface\\(s, &ctx->depth_surface, &ctx->depth_mask,\\n\"\n"
+    "    r\"                              &ctx->depth_function\\)\\) \\{\\n\"\n"
+    "    r\".*?\\n    \\})\\n    return true;\",\n"
+    "    r\"\"\"\\1\n"
+    "    ctx->texture_enabled = ctx->tex_control & ATI_3D_TEXMAP_ENABLE;\n"
+    "    if (ctx->texture_enabled && !ati_3d_texture_init(ctx)) {\n"
+    "        return false;\n"
+    "    }\n"
+    "    return true;\"\"\",\n"
+    ")\n"
 )
-'''
 code = code[:start] + replacement + code[end:]
 exec(compile(code, str(patch), "exec"))
